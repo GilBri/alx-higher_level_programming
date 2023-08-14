83bc4e61ca54e2c8170cba899dd93985bd22bca4
GilBri <bekiprop@gmail.com>
2023-08-14 20:39:07 +0300

    fix typo

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-	multiples_of_2 = [0]
+	multiples_of_2 = []
 	for num in my_list:
 		multiples_of_2.append(num % 2 == 0)
 		return multiples_of_2
