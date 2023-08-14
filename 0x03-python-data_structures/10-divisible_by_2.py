b3c118c1d5f918a28d1f7bdf03c44597758813a2
GilBri <bekiprop@gmail.com>
2023-08-14 20:35:58 +0300

    modify function to check for null list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for num in my_list:
-        result.append(num % 2 == 0)
-    return result
+	if my_list is None or len(my_list) == 0:
+		return None
+		
+	multiples_of_2 = my_list[0]
+	for num in my_list:
+		multiples_of_2.append(num % 2 == 0)
+		return multiples_of_2
