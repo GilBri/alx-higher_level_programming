001b56cecfb7bebbe0daf62b7e3b46f6c6d0700d
GilBri <bekiprop@gmail.com>
2023-08-14 20:41:58 +0300

    refactor code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-	multiples_of_2 = []
-	for num in my_list:
-		multiples_of_2.append(num % 2 == 0)
-		return multiples_of_2
+    result = []
+    for num in my_list:
+        result.append(num % 2 == 0)
+    return result
