09474a2abc3a1647421d6e232cbcef534863c3cc
GilBri <bekiprop@gmail.com>
2023-08-14 20:38:21 +0300

    remove null

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-	if my_list is None or len(my_list) == 0:
-		return None
-		
 	multiples_of_2 = [0]
 	for num in my_list:
 		multiples_of_2.append(num % 2 == 0)
