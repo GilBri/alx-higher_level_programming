50584b0c3fa62f4d8b475e0d26f4d53cbe1bdbaa
GilBri <bekiprop@gmail.com>
2023-08-14 20:37:17 +0300

    initialize

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
 	if my_list is None or len(my_list) == 0:
 		return None
 		
-	multiples_of_2 = my_list[0]
+	multiples_of_2 = [0]
 	for num in my_list:
 		multiples_of_2.append(num % 2 == 0)
 		return multiples_of_2
